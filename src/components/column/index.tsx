3f3e2c5efd6690712f2941fa8af96efbf2c8f4e1
Jude Carine <wslanxiaoxing@gmail.com>
2025-03-28 18:08:31 +0800

    feat:update title

--- src/components/column/index.tsx
+++ src/components/column/index.tsx
@@ -10,7 +10,7 @@ export function Column({ id }: { id: FixedColumnID }) {
     setCurrentColumnID(id)
   }, [id, setCurrentColumnID])
 
-  useTitle(`NewsNow | ${metadata[id].name}`)
+  useTitle(`Joey' network | ${metadata[id].name}`)
 
   return (
     <>
