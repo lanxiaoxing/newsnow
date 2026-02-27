93ca258e7147ebf9b0dd9dd35d69230e5a59afce
lanxiaoxing <wslanxiaoxing@gmail.com>
2026-02-27 11:23:36 +0800

    fix: Add browser-like headers to the `guozaoke.com` fetch request.

--- server/sources/guozaoke.ts
+++ server/sources/guozaoke.ts
@@ -2,7 +2,20 @@ import * as cheerio from "cheerio"
 import type { NewsItem } from "@shared/types"
 
 export default defineSource(async () => {
-  const response: string = await myFetch("https://www.guozaoke.com/")
+  const response: string = await myFetch("https://www.guozaoke.com/", {
+    headers: {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-User": "?1",
+      "Upgrade-Insecure-Requests": "1",
+      "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+    },
+  })
   if (!response) return []
 
   const $ = cheerio.load(response)
